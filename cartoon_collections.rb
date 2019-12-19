36379a4cfca5a5145c8a43a3acbcf7ddfbe53703
Rick Lim <rick.jason.a.lim@gmail.com>
2019-12-19 04:09:09 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -16,7 +16,10 @@ def long_planeteer_calls(array)
   }
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  cheese_types.each do|cheese|
+    return cheese if array.include?(cheese) 
+  end
+  nil
 end
