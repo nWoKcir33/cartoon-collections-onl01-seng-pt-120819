626c46a0cb09f8587891c018fb2200eae1012abb
Rick Lim <rick.jason.a.lim@gmail.com>
2019-12-19 04:08:54 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -4,8 +4,10 @@ def roll_call_dwarves(array)# code an argument here
   end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(array)
+  array.collect do|string|
+    string.capitalize + "!"
+  end
 end
 
 def long_planeteer_calls# code an argument here
